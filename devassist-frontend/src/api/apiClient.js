d3d189f60355dd3c9ff80c2cf7a14c409988cf87
Rajibul Mondal <rajibulmondal173@gmail.com>
2026-06-08 03:08:41 +0530

    fix: update BASE_URL to use correct environment variable for API

--- devassist-frontend/src/api/apiClient.js
+++ devassist-frontend/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: BASE_URL,
+  baseURL: import.meta.env.VITE_BASE_API,
 });
 
 apiClient.interceptors.request.use((config) => {
